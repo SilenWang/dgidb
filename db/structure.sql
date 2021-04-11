8e3db7020167c6b485e436fa12e7049ad82d756a
Silen Wang <qiumin14@163.com>
2021-04-12 01:32:43 +0800

    update sql file to solve compatibility problem

--- db/structure.sql
+++ db/structure.sql
@@ -1,13 +1,13 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
+-- SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
-SET row_security = off;
+-- SET row_security = off;
 
 --
 -- Name: clean(text); Type: FUNCTION; Schema: public; Owner: -
@@ -26,7 +26,7 @@ CREATE FUNCTION public.clean(text) RETURNS text
 
 SET default_tablespace = '';
 
-SET default_table_access_method = heap;
+-- SET default_table_access_method = heap;
 
 --
 -- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
